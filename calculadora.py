d036347810d483613506b2b296a00fbf8d7bf040
john mendes <131309978+Johnw07@users.noreply.github.com>
2023-10-09 21:21:51 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+numero1= int(input("Infrome o primeiro número: "))
+numero2= int(input("Informe o segundo número: "))
+
+print('{} + {}='.format(numero1, numero2))
+print(numero1 + numero2)
+
+print('{} - {}= '.format(numero1, numero2))
+print(numero1 - numero2)
+
+print('{} * {}= '.format(numero1, numero2))
+print(numero1 * numero2)
+
+print(' {} / {}= '.format(numero1, numero2))
+print(numero1 / numero2)
