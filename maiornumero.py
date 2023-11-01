ee8aa3a859b1a5772c1b96586627116e9b4e90f6
john mendes <131309978+Johnw07@users.noreply.github.com>
2023-11-01 20:45:44 -0300

    Add files via upload

---
+++ maiornumero.py
@@ -0,0 +1,10 @@
+x1 = int(input("Escreva o primeiro numero:"))
+x2 = int(input("Escreva o segundo numero:"))
+x3 = int(input("Escreva o terceiro numero:"))
+
+if x1 > x2 and x3:
+    print("o numero",x1,"é o maior")
+elif x2 > x3 and x1:
+    print("o numero",x2,"é o maior")   
+else:
+    print("o numero",x3,"é o maior")
