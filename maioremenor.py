d22e13bcfcd2a29216867c3fe1dc0c4e85f62fbc
john mendes <131309978+Johnw07@users.noreply.github.com>
2023-11-01 22:20:20 -0300

    Add files via upload

---
+++ maioremenor.py
@@ -0,0 +1,21 @@
+x1 = int(input("Escreva o primeiro numero:"))
+x2 = int(input("Escreva o segundo numero:"))
+x3 = int(input("Escreva o terceiro numero:"))
+
+if x1 < x3 and x2 :
+     print("o numero",x1,"é o menor")
+elif x2 < x3 and x1 :
+    print("o numero",x2,"é o menor")
+else :
+    print("o numero",x3,"é o menor")
+
+x4 = int(input("Escreva o primeiro numero:"))
+x5 = int(input("Escreva o segundo numero:"))
+x6 = int(input("Escreva o terceiro numero:"))
+
+if x4 > x6 and x5 :
+     print("o numero",x4,"é o maior")
+elif x5 > x6 and x4 :
+    print("o numero",x5,"é o maior")
+else :
+    print("o numero",x6,"é o maior")
