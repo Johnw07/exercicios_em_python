e88b901faa2cc88558ed0ce6265d36c319f5afb0
john mendes <131309978+Johnw07@users.noreply.github.com>
2024-04-16 18:57:03 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,4 @@
-numero1= int(input("Infrome o primeiro número: "))
+numero1= int(input("Informe o primeiro número: "))
 numero2= int(input("Informe o segundo número: "))
 
 print('{} + {}='.format(numero1, numero2))
